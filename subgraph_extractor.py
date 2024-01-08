5410c26aaaf3f2b1a24214ee2bcb1bf50c37adef
ISI <aishikchanda11@gmail.com>
2024-01-08 14:12:10 +0530

    updated main method

--- subgraph_extractor.py
+++ subgraph_extractor.py
@@ -21,7 +21,7 @@ import argparse
 from sklearn.model_selection import KFold
 
 print(device)
-matplotlib.use("TkAgg")
+#matplotlib.use("TkAgg")
 
 def train(model, opt, curr_epoch, train_dataloader, val_loader, name, epochs):
     loss_func = F.nll_loss
@@ -70,7 +70,7 @@ def train(model, opt, curr_epoch, train_dataloader, val_loader, name, epochs):
 
                 correct += torch.sum(torch.eq(F.softmax(score, dim=-1).max(dim=-1)[1], label.squeeze().to(device)),
                                      -1).item()
-                #running_loss_train.append(loss.item())
+                running_loss_test.append(loss.item())
         val_acc = correct / len(val_loader.dataset)
         print('\ntrain accuracy = ' + str(train_acc) + '  val accuracy ' + str(val_acc))
 
@@ -95,12 +95,60 @@ def train(model, opt, curr_epoch, train_dataloader, val_loader, name, epochs):
             plt.ylabel("Y axis")
             plt.plot(x, train_epoch_loss, color="red")
             plt.plot(x, test_epoch_loss, color="blue")
-            plt.draw()
+            plt.show()
             print(epoch_loss_train)
 
         print('---------------fold-' + str(foldc) + '-------- ecpoch ' + str(epoch) + " ------------------")
     return model
 
+def start(dataset='MUTAG', dataset_feat='attr', dataset_multiplier=3, dw_dim=32, dw_walk_length= 10, dw_window_size=4, model_name='model_1', pool=True, epoch=800, hout=128, kfold=False):
+    feat_key = dataset_feat
+    if dataset == 'MUTAG':
+        data = GINDataset('MUTAG', self_loop=True)
+
+    data = utils.prep_data(data, feat_key, dw_dim, dw_walk_length, dw_window_size) ##Deepwalk
+    data = np.array(data)
+    print("Data prepared")
+
+    kf = KFold(n_splits=10, random_state=1, shuffle=True)
+    if kfold:
+      k = 10
+      folds = list(kf.split(data))[:]
+    else:
+      k = 1
+      folds = list(kf.split(data))[:1]
+
+
+
+    for train_ids, val_ids in folds:
+
+        train_set = data[train_ids]
+        val_set = data[val_ids]
+
+        labels = set()
+
+        for g,l in data:
+            labels.add(l.item())
+        labels = list(labels)
+        labels.sort()
+        print(labels)
+
+        train_data = utils.get_supervised_dataloader(train_set,dataset_multiplier, feat_key, labels)
+        val_data = utils.get_supervised_dataloader(val_set,dataset_multiplier, feat_key, labels)
+
+        train_dataloader = GraphDataLoader(train_data, batch_size=64, shuffle=True)
+        val_dataloader = GraphDataLoader(val_data, batch_size=64, shuffle=True)
+
+        model = pre_embedding(39,hout, len(labels)).float().to(device) #todo
+        opt = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.001)
+        model = train(model, opt, 0, train_dataloader, val_dataloader, model_name, epoch)
+
+        pooler = edgepooling_training(model, len(labels))
+
+        if pool:
+            for g,l in tqdm(train_data):
+                feats = g.ndata['feat_onehot'].to(device)
+                outs, nlclus_list, pcluster_list, pooled_graph_list = pooler(g, feats.detach().float())
 
 if __name__ == '__main__':
 
@@ -123,53 +171,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    feat_key = args.dataset_feat
-    if args.dataset == 'MUTAG':
-        data = GINDataset('MUTAG', self_loop=True)
-
-    data = utils.prep_data(data, feat_key, args.dw_dim, args.dw_walk_length, args.dw_window_size) ##Deepwalk
-    data = np.array(data)
-    print("Data prepared")
-
-    if args.kfold:
-        folds_id = list(range(10))
-        kf = KFold(n_splits=10, random_state=1, shuffle=True)
-
-    else:
-        folds_id = [0]
-        kf = KFold(n_splits=1)
+    start(args.dataset, args.dataset_feat, args.dataset_multiplier, args.dw_dim, args.dw_walk_length, args.dw_window_size, args.model_name, args.pool, args.epoch, args.hout, args.kfold)
 
 
-
-    for train_ids, val_ids in kf.split(data):
-
-        train_set = data[train_ids]
-        val_set = data[val_ids]
-
-        labels = set()
-
-        for g,l in data:
-            labels.add(l.item())
-        labels = list(labels)
-        labels.sort()
-        print(labels)
-
-        train_data = utils.get_supervised_dataloader(train_set,args.dataset_multiplier, feat_key, labels)
-        val_data = utils.get_supervised_dataloader(val_set,args.dataset_multiplier, feat_key, labels)
-
-        train_dataloader = GraphDataLoader(train_data, batch_size=64, shuffle=True)
-        val_dataloader = GraphDataLoader(val_data, batch_size=64, shuffle=True)
-
-        model = pre_embedding(39,args.hout, len(labels)).float().to(device) #todo
-        opt = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.001)
-        model = train(model, opt, 0, train_dataloader, val_dataloader, args.model_name, args.epoch)
-
-        pooler = edgepooling_training(model, len(labels))
-
-        if args.pool:
-            for g,l in tqdm(train_data):
-                feats = g.ndata['feat_onehot'].to(device)
-                outs, nlclus_list, pcluster_list, pooled_graph_list = pooler(g, feats.detach().float())
 
 
 
